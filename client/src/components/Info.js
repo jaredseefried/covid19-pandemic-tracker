7cbb1c3f726e6a161e182df24159285008f975cd
Jared Seefried <jared.seefried@yahoo.com>
2020-12-14 12:45:25 -0700

    playing with styling

--- client/src/components/Info.js
+++ client/src/components/Info.js
@@ -4,10 +4,10 @@ function Info(props) {
   return (
     <div className="info-container">
       <h1 className="info-title">{props.country}</h1>
-      <h3 className="info">Infected: {props.infected}</h3>
-      <h3 className="info">Deaths: {props.deaths}</h3>
-      <h3 className="info">Recoveries: {props.recoveries}</h3>
-      <h3 className="info">Last Updated: {props.updated}</h3>
+      <h3 className="info"><strong>Infected:</strong> {props.infected}</h3>
+      <h3 className="info"><strong>Deaths:</strong> {props.deaths}</h3>
+      <h3 className="info"><strong>Recoveries:</strong> {props.recoveries}</h3>
+      <h3 className="info"><strong>Updated:</strong> {props.updated}</h3>
     </div>
   )
 }
